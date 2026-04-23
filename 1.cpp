ae4b4634468f5850adb95e2f457ebd7ffe3dab72
vvd-merge <v_desyatkina@inbox.ru>
2026-04-23 18:36:36 +0700

    1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
 
+int main() {
+    std::cout << "trapezoid\n";
+
+    double a, b, c, d, h;
+
+    //функция для ввода положительного числа с проверкой на ошибки
+    auto input = [](const char* trapezoid) {
+        double a;
+        do {
+            std::cout << trapezoid << " = ";
+            std::cin >> a;
+            if (std::cin.fail() || a <= 0) {
+                std::cout << "ERROR\n";
+                std::cin.clear();
+                std::cin.ignore(10000, '\n');
+                a = -1;
+            }
+        } while (a <= 0);
+        return a;
+        };
+
+    a = input("Bottom base");   // нижнее основание
+    b = input("Top base");      // верхнее основание
+    c = input("Left side");     // левая боковая сторона
+    d = input("Right side");    // правая боковая сторона
+    h = input("Height");        // высота
+
+    std::cout << "Perimeter = " << a + b + c + d << "\n"
+        << "Area = " << ((a + b) / 2) * h << "\n"
+        << "Midline length = " << (a + b) / 2 << std::endl;
+}
